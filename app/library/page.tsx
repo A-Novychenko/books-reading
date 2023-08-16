127270ea9a762dcf29c895ba6f42a0600d1bbc2a
alexveryk <veryk.av@gmail.com>
2023-08-17 00:12:33 +0300

    Added a library component and three internal components and styles

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
-import {FC} from "react";
+import { FC } from "react";
 
 // Start
-import {Info} from "@/components/Library/Info/Info";
-import {Books} from "@/components/Library/BooksLibrary/BooksLibrary";
+import { Info } from "@/components/Library/Info/Info";
+import { Library } from "@/components/Library/Library";
+
 // End
 
 const LibraryPage: FC = () => {
   return (
     <div>
       <h1 className="visually-hidden">Library</h1>
-      <Info />
+      {/* <Info /> */}
+      <Library />
     </div>
   );
 };
