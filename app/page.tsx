c318562a28f0336678c1fb3475f7a8792e6e907d
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-08-20 13:24:45 +0300

    add loader and getServerSession-redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
-import Link from "next/link";
-
-import { ConfirmBtn } from "@/components/Auth/ConfirmBtn/ConfirmBtn";
+// import Link from "next/link";
+import {ConfirmBtn} from "@/components/Auth/ConfirmBtn/ConfirmBtn";
 
 import styles from "../styles/page.module.scss";
 
 export default function Home() {
-
   return (
     <main className={styles.main}>
-      <Link href="/signin">To AUTH</Link>
+      {/* <Link href="/signin">To AUTH</Link> */}
       <h1 className="visually-hidden">Books read home page</h1>
       <ConfirmBtn />
     </main>
